35f21a09bcba199197a785b0a00f9a3fd5b28f60
param-ninthu <ninthu1999@gmail.com>
2023-06-11 03:24:13 +0530

    Readme file has been updated

---
+++ rest/Readme.md
@@ -0,0 +1,19 @@
+# Google Calendar API Integration
+
+This is the backend program for to fetch the busy intervals in a given period of time which shared in the Google calendar.
+
+## Requirements
+
+- **NodeJS**
+- **ExpressJS**
+- **Create Google Cloud Project and Enable Google Calender API**
+- **Add the variables and configurations of your Google Cloud Project**
+
+### Click the following link for the [Reference]("https://developers.google.com/calendar/api/quickstart/nodejs")
+
+## Output
+
+Check for below Screenshots as the reference of the **output**.
+
+![ALT_img](./screenshot/testing.png)
+![ALT_img](./screenshot/output.png)
--- rest/controller/eventController.js
+++ rest/controller/eventController.js
@@ -6,29 +6,31 @@ require("dotenv").config();
 //@access Public
 
 const fetchEvents = async (req, res) => {
+  // here we are extracting the calendar_Id, startTime and endTime from the request body
   const { calendar_Id, startTime, endTime } = req.body;
 
+  // here we are creating a new GoogleAuth object for authentication
   const auth = new google.auth.GoogleAuth({
-    keyFile: "./cred.json",
-    scopes: ["https://www.googleapis.com/auth/calendar.readonly"],
+    keyFile: "./cred.json", // path to the credential files
+    scopes: ["https://www.googleapis.com/auth/calendar.readonly"], // scopes for calendar access
   });
 
-  const calendar = google.calendar({ version: "v3", auth });
+  const calendar = google.calendar({ version: "v3", auth }); // Here we are creating the new calendar instance
 
   try {
     const response = await calendar.events.list({
-      auth: auth,
-      calendarId: calendar_Id,
+      auth: auth, // authenticating the request
+      calendarId: calendar_Id, // using this calendar id it will fetch the events
       timeMin: startTime,
       timeMax: endTime,
       timeZone: "Asia/Kolkata",
     });
 
     const items = response.data.items;
-    res.json(items);
+    res.json(items); // sending the response as json
   } catch (error) {
     console.log(`Error at fetchEvents --> ${error}`);
-    res.status(500).json({ error: "Failed to fetch events" });
+    res.status(500).json({ error: "Failed to fetch events" }); // shoes the status code with error message
   }
 };
 
