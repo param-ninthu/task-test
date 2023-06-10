ebe61d946abf347cff17cd1b96a7c92d6c99d547
param-ninthu <ninthu1999@gmail.com>
2023-06-11 03:26:17 +0530

    ..

--- rest/server.js
+++ rest/server.js
@@ -5,10 +5,6 @@ const app = express();
 
 const PORT = process.env.PORT || 5001;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 app.use(express.json());
 app.use("/api/events", require("./routes/eventRoutes"));
 
